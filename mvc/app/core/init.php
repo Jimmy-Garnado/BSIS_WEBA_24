8280787d5e8fd179f9545e4a0144917558bf008a
Jimmy-Garnado <jimmygarnado23n@gmail.com>
2024-03-22 16:15:13 +0800

    updated_mvc

--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -2,3 +2,13 @@
 
 require 'functions.php';
 require 'App.php';
+require 'Database.php';
+require 'Controller.php';
+require 'Model.php';
+require 'App.php';
+
+
+spl_autoload_register(function($class_name)
+{
+    require '../app/models' . $class_name . ' .php';
+});
