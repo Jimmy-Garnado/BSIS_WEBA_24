cf4120e4e93f49184c117200afa1d13ccf445053
Jimmy-Garnado <jimmygarnado23n@gmail.com>
2024-04-23 22:06:35 +0800

    list-of-task

---
+++ mvc-test/app/core/init.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'functions.php';
+require 'App.php';
+require 'Database.php';
+require 'Controller.php';
+require 'Model.php';
+require 'App.php';
+
+
+spl_autoload_register(function($class_name)
+{
+    require '../app/models' . $class_name . ' .php';
+});
