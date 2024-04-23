2d4bc30c5ced830901742b4e879b4ac78783afce
Jimmy-Garnado <jimmygarnado23n@gmail.com>
2024-04-23 22:24:36 +0800

    list-of-task

---
+++ list-of-task/app/core/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('APP_NAME', 'Task');
+
+define('ROOT', 'http://localhost/bsis3f_weba_24/list-of-task/public');
+define('PATH', 'C:/xampp/htdocs/BSIS3F_WEBA_24/list-of-task/app/views/');
+
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'tasks_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
